179a683ab67644b6d9ba8d95cd50dda9f1c296a2
zmarci1101 <zmarci1101@users.noreply.github.com>
2021-01-16 17:08:44 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,6 +4,8 @@ public class Main{
         System.out.println(n);
     }
     
+    valami
+    
     public static void main(String[] args){
         System.out.println("Valami");
         kiirInt(2);
