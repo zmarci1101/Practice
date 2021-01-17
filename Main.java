b586c1bc093296b8c5d46f712123a5ae37ed5553
Zohó Marcell <zmarci1101@gmail.com>
2021-01-17 14:10:55 +0100

    Bugfixes

--- Main.java
+++ Main.java
@@ -3,9 +3,7 @@ public class Main{
     private static void kiirInt(int n){
         System.out.println(n);
     }
-    
-    valami
-    v
+   
     
     public static void main(String[] args){
         System.out.println("Valami");
