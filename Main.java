24cbe09cfe1e91e04a493b531a466a6ce4b6f42f
Zohó Marcell <zmarci1101@gmail.com>
2021-01-17 14:47:51 +0100

    B test

--- Main.java
+++ Main.java
@@ -3,6 +3,8 @@ public class Main{
     private static void kiirInt(int n){
         System.out.println(n);
     }
+
+    b1
     
     public static void main(String[] args){
         System.out.println("Valami");
