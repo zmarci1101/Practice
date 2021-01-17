f81980ea7f8e7d104efc699a0cb879c0e8aaa189
Zohó Marcell <zmarci1101@gmail.com>
2021-01-17 14:57:12 +0100

    Other 2 branch

--- Main.java
+++ Main.java
@@ -3,6 +3,10 @@ public class Main{
     private static void kiirInt(int n){
         System.out.println(n);
     }
+
+
+
+    b3
     
     public static void main(String[] args){
         System.out.println("Valami");
